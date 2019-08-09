4a86e618e6bfbcdadab5f81ca06f3588090521dc
= <=>
2019-08-09 14:24:48 -0400

    added code for rediret and make response

--- Notes/FlaskNotes.md
+++ Notes/FlaskNotes.md
@@ -3,3 +3,18 @@
 > Create a .flaskenv file to execute the flask app
 > FLASK_APP=main.py (Application to run)
 > FLASK_ENV=development/production (set to development for debug messages)
+
+
+### Command lines
+> To view the list of routes type the following command 
+> flask routes 
+ 
+|Endpoint      | Methods | Rule                   |
+|--------------| ------- | -----------------------|
+|create_error  | GET     |/error                  |
+|greeting      | GET     |/greeting/<name>        |
+|index         |  GET    | /index                 |
+|index         |  GET    | /home                  |
+|index         | GET     |/                       |
+|return_message|  GET    | /data                  |
+|static        | GET     |/static/<path:filename> |
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, make_response
+from flask import Flask, Response, make_response,redirect
 
 app = Flask(__name__)
 
@@ -9,5 +9,26 @@ app = Flask(__name__)
 def index():
    return "Welcome to Programming in Flask"
 
+@app.route("/greeting/<name>")
+def greeting(name:str):
+    
+    response = make_response(f"<h3>Welcome to coding with Flask, {name.capitalize()}")
+    return response
 
+@app.route('/error')
+def create_error():
+   resp = make_response("This is an error page!!",404)
+   return resp
+
+#Creating a cookie
+@app.route('/data')
+def return_message():
+   response = make_response("<h3>I am going to set the cookie for the session</h3>",200)
+   response.set_cookie("poll_interval","42")
+   return response
+
+#Creating a redirect
+@app.route('/youtube')
+def gotoyoutube():
+   return redirect("http://youtube.com")
 
